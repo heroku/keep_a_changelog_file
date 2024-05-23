cb475305265cfaddac9289d6af232b584b683c4f
Colin Casey <casey.colin@gmail.com>
2024-05-23 16:46:48 -0300

    Test wasm action

--- validate/index.js
+++ validate/index.js
@@ -1,10 +1,10 @@
 const core = require('@actions/core');
 const glob = require('@actions/glob');
 
-try {
+main().catch(e => core.setFailed(e.message))
+
+async function main() {
     const globber = await glob.create(core.getInput('changelogs'))
     const changelogs = await globber.glob()
     console.log(`Changelogs: ${changelogs.join("\n")}`)
-} catch (error) {
-    core.setFailed(error.message);
 }
