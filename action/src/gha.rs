c28aab53af79730b6c34bc578e1f0c9a9bc91b2d
Colin Casey <casey.colin@gmail.com>
2025-02-17 12:33:16 -0400

    Testing action

--- action/src/gha.rs
+++ action/src/gha.rs
@@ -1,3 +1,9 @@
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
+use std::io::stdout;
+use std::ops::Deref;
+use std::path::PathBuf;
+
 #[bon::builder]
 pub(crate) fn get_input(
     #[builder(start_fn)] //
@@ -52,4 +58,131 @@ pub(crate) fn get_multiline_input(
 
 pub(crate) enum InputError {
     MissingRequiredValue(String),
+}
+
+#[bon::builder]
+pub(crate) fn error(
+    #[builder(start_fn, into)] //
+    message: String,
+    #[builder(into)] //
+    title: Option<String>,
+    file: Option<PathBuf>,
+    start_line: Option<usize>,
+    end_line: Option<usize>,
+    start_column: Option<usize>,
+    end_column: Option<usize>,
+) {
+    issue_command(Command {
+        command: "error".to_string(),
+        properties: AnnotationProperties::builder()
+            .maybe_title(title)
+            .maybe_file(file)
+            .maybe_start_line(start_line)
+            .maybe_end_line(end_line)
+            .maybe_start_column(start_column)
+            .maybe_end_column(end_column)
+            .build()
+            .into(),
+        message,
+    });
+}
+
+fn issue_command(command: Command) {
+    println!("{command}");
+}
+
+struct Command {
+    command: String,
+    message: String,
+    properties: CommandProperties,
+}
+
+const CMD_STRING: &str = "::";
+
+impl Display for Command {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{CMD_STRING}{}", self.command)?;
+        if !self.properties.is_empty() {
+            write!(f, " ")?;
+        }
+        let mut property_strings = vec![];
+        for (key, value) in self.properties.deref() {
+            property_strings.push(format!("{}={}", key, escape_property(value)));
+        }
+        write!(f, "{}", property_strings.join(","))?;
+        write!(f, "{CMD_STRING}{}", escape_data(&self.message))
+    }
+}
+
+struct CommandProperties {
+    inner: HashMap<String, String>,
+}
+
+impl Deref for CommandProperties {
+    type Target = HashMap<String, String>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+#[derive(bon::Builder)]
+struct AnnotationProperties {
+    title: Option<String>,
+    file: Option<PathBuf>,
+    start_line: Option<usize>,
+    end_line: Option<usize>,
+    start_column: Option<usize>,
+    end_column: Option<usize>,
+}
+
+impl From<AnnotationProperties> for CommandProperties {
+    fn from(props: AnnotationProperties) -> Self {
+        let mut inner = HashMap::new();
+        if let Some(title) = props.title {
+            inner.insert("title".to_string(), escape_property(&title));
+        }
+        if let Some(file) = props.file {
+            inner.insert("file".to_string(), escape_property(&file.to_string_lossy()));
+        }
+        if let Some(start_line) = props.start_line {
+            inner.insert(
+                "startLine".to_string(),
+                escape_property(&start_line.to_string()),
+            );
+        }
+        if let Some(end_line) = props.end_line {
+            inner.insert(
+                "endLine".to_string(),
+                escape_property(&end_line.to_string()),
+            );
+        }
+        if let Some(start_column) = props.start_column {
+            inner.insert(
+                "startColumn".to_string(),
+                escape_property(&start_column.to_string()),
+            );
+        }
+        if let Some(end_column) = props.end_column {
+            inner.insert(
+                "endColumn".to_string(),
+                escape_property(&end_column.to_string()),
+            );
+        }
+        CommandProperties { inner }
+    }
+}
+
+fn escape_data(data: &str) -> String {
+    data.replace("%", "%25")
+        .replace("\r", "%0D")
+        .replace("\n", "%0A")
+}
+
+fn escape_property(data: &str) -> String {
+    data.replace("%", "%25")
+        .replace("\r", "%0D")
+        .replace("\n", "%0A")
+        .replace(":", "%3A")
+        .replace(",", "%2C")
 }
--- action/src/main.rs
+++ action/src/main.rs
@@ -2,6 +2,11 @@ mod gha;
 
 use crate::gha::{get_multiline_input, InputError};
 use glob::{glob, PatternError};
+use keep_a_changelog_file::Changelog;
+use std::fs;
+use std::fs::DirEntry;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 fn main() {
     println!("Hello, world!");
@@ -14,18 +19,46 @@ fn execute_action() -> Result<(), ActionError> {
     let changelog_file_globs = get_multiline_input("changelog_files")
         .call()
         .map_err(ActionError::Input)?;
+
     for changelog_file_glob in changelog_file_globs {
         for entry in glob(&changelog_file_glob)
             .map_err(ActionError::Pattern)?
             .filter_map(Result::ok)
         {
-            println!("{}", entry.to_string_lossy());
+            if entry.is_file() {
+                validate_changelog(&entry)?;
+            }
         }
     }
+
     Ok(())
+}
+
+fn validate_changelog(changelog_file: &Path) -> Result<(), ActionError> {
+    let contents = fs::read_to_string(changelog_file)
+        .map_err(|e| ActionError::ReadChangelog(changelog_file.to_path_buf(), e))?;
+
+    match Changelog::from_str(&contents) {
+        Ok(changelog) => {}
+        Err(diagnostics) => {
+            for diagnostic in diagnostics {
+                gha::error(&diagnostic.message)
+                    .title(&diagnostic.message)
+                    .file(changelog_file.to_path_buf())
+                    .start_line(diagnostic.position.start.line)
+                    .end_line(diagnostic.position.end.line)
+                    .start_column(diagnostic.position.start.column)
+                    .end_column(diagnostic.position.end.column)
+                    .call();
+            }
+        }
+    }
+
+    todo!()
 }
 
 enum ActionError {
     Input(InputError),
     Pattern(PatternError),
+    ReadChangelog(PathBuf, std::io::Error),
 }
